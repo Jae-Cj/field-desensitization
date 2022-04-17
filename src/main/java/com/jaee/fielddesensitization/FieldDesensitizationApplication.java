d3a63a3ddbd649e9a4d5d61ffb71b27ebae230e7
c-zhanhw01 <c-zhanhw01@vanke.com>
2022-04-17 13:46:09 +0800

    Feat:字段脱敏功能

---
+++ src/main/java/com/jaee/fielddesensitization/FieldDesensitizationApplication.java
@@ -0,0 +1,13 @@
+package com.jaee.fielddesensitization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldDesensitizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldDesensitizationApplication.class, args);
+    }
+
+}
